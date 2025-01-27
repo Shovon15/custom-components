4edb4b2e118bdab540b1f80fb4063710e69db02b
Shovon15 <shovonrnbd@gmail.com>
2025-01-27 18:01:23 +0600

    .

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,12 +1,28 @@
+'use client';
 import React from 'react';
 import SingleSlide from './components/SingleSlide/SingleSlide';
 import DoubleSlide from './components/DoubleSlide/DoubleSlide';
+// import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import Lottie from 'lottie-react';
+import animationData from './components/lottie-files/lottie-file.json';
 
 const HomePage = () => {
   return (
     <div>
       <SingleSlide />
       <DoubleSlide />
+      {/* <DotLottieReact
+      src="https://lottie.host/2ba68ff5-51df-499c-9aaa-e8764a1fb3b5/cQxdKwT12y.lottie"
+      loop
+      autoplay
+    /> */}
+
+      <Lottie
+        animationData={animationData}
+        loop={true}
+        style={{ width: 300, height: 300 }}
+        className=''
+      />
     </div>
   );
 };
